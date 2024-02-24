06da5cc2c0b0ca86b56ceb025f6510f92c6fa88f
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-02-24 12:10:53 -0300

    Revert "Run tests concurrently"

--- tests/test_pitz.py
+++ tests/test_pitz.py
@@ -16,7 +16,7 @@ async def pitz(tmp_path: Path) -> Case:
     return await PITZ.copy(tmp_path / PITZ.name)
 
 
-@pytest.mark.asyncio_cooperative
+@pytest.mark.asyncio
 @pytest.mark.parametrize("script", [None, False])
 async def test_run(pitz: Case, script: Optional[bool]) -> None:
     await pitz.run(script=script)
@@ -24,20 +24,20 @@ async def test_run(pitz: Case, script: Optional[bool]) -> None:
     await pitz.run(script=script)
 
 
-@pytest.mark.asyncio_cooperative
+@pytest.mark.asyncio
 async def test_double_clean(pitz: Case) -> None:
     await pitz.clean()
     await pitz.clean(check=True)
     await pitz.run()
 
 
-@pytest.mark.asyncio_cooperative
+@pytest.mark.asyncio
 async def test_run_script(pitz: Case) -> None:
     with pytest.raises(RuntimeError):
         await pitz.run(script=True)
 
 
-@pytest.mark.asyncio_cooperative
+@pytest.mark.asyncio
 @pytest.mark.parametrize("script", [None, False])
 async def test_run_parallel(pitz: Case, script: Optional[bool]) -> None:
     with pytest.raises(RuntimeError):
