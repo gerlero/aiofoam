adf78c5b62444ff394ad9bc7d6a945d8c3a3c620
Gabriel Gerlero <gerlero@users.noreply.github.com>
2024-02-25 10:35:35 -0300

    Bump version to 0.2.2

--- aiofoam/__init__.py
+++ aiofoam/__init__.py
@@ -9,7 +9,7 @@ from typing import Optional, Union, AsyncGenerator, Mapping, Set
 
 import aioshutil
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 max_cpus: int = multiprocessing.cpu_count()
 """
